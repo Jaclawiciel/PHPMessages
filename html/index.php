7079faf517bb0e4e973bf4f3f57b2616e83d7fa6
Jacek Gałka <contact@jacekgalka.pl>
2017-05-02 20:08:12 +0200

    Initial test commit

---
+++ html/index.php
@@ -0,0 +1,11 @@
+<html>
+  <head>
+    <title>PHP <3</title>
+    <link rel="stylesheet" href="../css/main.css" type="text/css">
+  </head>
+  <body>
+    <h1>Test</h1>
+
+    <?php include '../php/test.php';?>
+  </body>
+</html>

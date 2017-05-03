4800c2df2800b056692d2c7b8f38423d559e784b
Jacek Gałka <contact@jacekgalka.pl>
2017-05-03 16:06:20 +0200

    HTML structure ready and basic css

--- html/index.php
+++ html/index.php
@@ -4,14 +4,23 @@
     <link rel="stylesheet" href="../css/main.css" type="text/css">
   </head>
   <body>
-    <div>
-      <h1>PHPMessages</h1>
-      <h2>All messages</h2>
+    <div id="content">
+      <div id="header">
+        <h1>PHPMessages</h1>
+        <h2>All messages</h2>
+      </div>
       <form>
-        <textarea readonly>Test</textarea>
+        <textarea readonly cols="45" rows="5" name="message">
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut porttitor erat in massa posuere, eu porta mi lobortis. Proin ac velit quis justo ultrices malesuada id quis mi.
+        </textarea>
+        <textarea readonly cols="45" rows="5" name="message">
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut porttitor erat in massa posuere, eu porta mi lobortis. Proin ac velit quis justo ultrices malesuada id quis mi.
+        </textarea>
+        <textarea readonly cols="45" rows="5" name="message">
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut porttitor erat in massa posuere, eu porta mi lobortis. Proin ac velit quis justo ultrices malesuada id quis mi.
+        </textarea>
       </form>
+      <a id="buttonLink" href="admin.php">Admin Panel</a>
     </div>
-
-    <?php include '../php/test.php';?>
   </body>
 </html>
